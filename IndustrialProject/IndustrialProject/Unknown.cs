ea8d6c9a5d1be105e6505a8a4bbcf31f3234dfc6
purplegoat53 <jrose@dundee.ac.uk>
2016-09-14 12:37:31 +0100

    Parse Unknown (0xFA)

---
+++ IndustrialProject/IndustrialProject/Unknown.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace IndustrialProject
+{
+    public class Unknown 
+    {
+        // protocol id: 0xfa
+
+        public byte sequenceNumber;
+        public byte[] data;
+
+        // parses the Unknown packet data after the protocol id
+        public void parse(MemoryStream stream)
+        {
+            this.sequenceNumber = (byte)stream.ReadByte();
+            this.data = new byte[stream.Length - 1];
+            stream.Read(this.data, 0, stream.Length);
+        }
+    }
+}
