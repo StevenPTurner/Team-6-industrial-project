4e4fa0e6463b33c5678b857ff9a894deefba624e
purplegoat53 <jrose@dundee.ac.uk>
2016-09-15 11:40:55 +0100

    Error On Empty Unknown (0xFA) Packet

--- IndustrialProject/IndustrialProject/Unknown.cs
+++ IndustrialProject/IndustrialProject/Unknown.cs
@@ -16,6 +16,11 @@ namespace IndustrialProject
         // parses the Unknown packet data after the protocol id
         public override Packet.ErrorType parseAndCheck(MemoryStream stream)
         {
+            if (stream.Position + 1 > stream.Length)
+            {
+                return Packet.ErrorType.ERROR_NOT_ENOUGH_BYTES;
+            }
+
             this.sequenceNumber = (byte)stream.ReadByte();
             this.data = new byte[stream.Length - 1];
             stream.Read(this.data, 0, (int)stream.Length);
