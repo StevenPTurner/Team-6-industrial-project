69175a46373027e7b2b5216ee1edd3da7e67bafb
StevenPTurner <spt10pd@msn.com>
2016-09-15 10:05:47 +0100

    added packet,stats and file class, implimented basic features for file

---
+++ IndustrialProject/IndustrialProject/File.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndustrialProject
+{
+    class File
+    {
+        String filename { set; get; }
+        DateTime startDate { set; get; }
+        int port { set; get; }
+        List<Packet> packet;
+        Stats stats;
+
+        public File()
+        {
+            packet = new List<Packet>();
+        }
+
+        public void addPacket(Packet packetToAdd)
+        {
+            packet.Add(packetToAdd);
+        }
+    }
+}
