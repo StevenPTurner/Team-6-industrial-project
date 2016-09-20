a6ab256a13d2b9dd3fe64a928a6398e2e09ab997
Connor Sewell <ctrsewell@dundee.ac.uk>
2016-09-20 11:22:52 +0100

    Creating GraphCalculation class.

---
+++ IndustrialProject/IndustrialProject/GraphCalculations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndustrialProject
+{
+    class GraphCalculations
+    {
+        public double calcPacketTimeDif(DateTime date1, DateTime date2)
+        {
+            double timeDifference = 0;
+            return timeDifference = (date2 - date1).TotalSeconds;
+        }
+
+
+            
+
+    }
+}
